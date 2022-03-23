7790ff55159bbab9b6b6942bdf997119e455a346
mincudini <m.incudini@reply.it>
2022-03-23 16:33:42 +0100

    ntk simulations

--- linear-teacher/main-linear-teacher-jax.py
+++ linear-teacher/main-linear-teacher-jax.py
@@ -1,11 +1,12 @@
 import pennylane as qml
-from pennylane import numpy as pnp  # -> substituted by JAX
+import jax
+import jax.numpy as jnp
 from pennylane.optimize import AdamOptimizer, GradientDescentOptimizer
 import pandas as pd
 from functools import partial
-import matplotlib.pyplot as plt
 import json
 from datetime import datetime
+
 
 class PennylaneLinearTeacher:
 
@@ -13,8 +14,10 @@ class PennylaneLinearTeacher:
 
         self.n_layers = n_layers
         self.device = qml.device("default.qubit", wires=1)
+        self.rng = jax.random.PRNGKey(593539)
 
-        @qml.qnode(self.device)
+        @jax.jit
+        @qml.qnode(self.device, interface="jax")
         def one_qubit_learner(x, theta):
             qml.RY(x, wires=0)
             for i, thetai in enumerate(theta):
@@ -28,34 +31,39 @@ class PennylaneLinearTeacher:
         self.one_qubit_learner = one_qubit_learner
 
     def generate_dataset(self, size, noise=0):
-        X = pnp.random.uniform(low=0, high=1, size=(size,))
-        Y = X * self.linear_w + pnp.random.uniform(low=-noise, high=+noise, size=(size,))
+        X = jax.random.uniform(self.rng, minval=0, maxval=1, shape=(size,))
+        Y = X * self.linear_w + jax.random.uniform(self.rng, minval=-noise, maxval=+noise, shape=(size,))
         return X, Y
 
-    def evaluate(self, initial_params, optimizer, epochs, X_train, Y_train, X_test, Y_test):
+    def evaluate(self, initial_params, epochs, X_train, Y_train, X_test, Y_test):
 
         specs = {'initial_params': str(initial_params),
-                 'optimizer': str(type(optimizer)),
+                 'optimizer': 'jax_gradient_flow',
                  'epochs': epochs,
                  'layers': self.n_layers,
                  'linear_w': self.linear_w,
                  'X_train': str(X_train),
                  'Y_train': str(Y_train),
                  'X_test': str(X_test),
-                 'Y_test': str(Y_test)}
+                 'Y_test': str(X_test)}
 
         df = pd.DataFrame(columns=['epoch', 'training_loss', 'testing_loss', 'params'])
 
         def get_mse_loss(X, Y, params):
-            return sum((self.one_qubit_learner(X[i], params) - Y[i])**2 for i in range(X.shape[0]))
+            cost = [(self.one_qubit_learner(X[index], params) - Y[index])**2 for index in range(X.shape[0])]
+            jax_cost = jnp.array(cost)
+            jax_sum = jnp.sum(jax_cost)
+            return jax_sum
 
         training_loss = partial(get_mse_loss, X_train, Y_train)
+        training_loss_grad = jax.grad(training_loss)
         testing_loss = partial(get_mse_loss, X_test, Y_test)
         params = initial_params
 
         for i in range(epochs+1):
 
-            if i > 0: params = optimizer.step(training_loss, params)
+            if i > 0:
+                params -= training_loss_grad(params)
 
             df.loc[len(df)] = {
                 'epoch': i,
@@ -77,18 +85,17 @@ training_losses = []
 testing_losses = []
 
 def main():
-    for v in range(3, 20+1):
+    for v in range(21, 21+1):
         for l in range(MAX_DEPTH):
             l = l + 1
             plteacher = PennylaneLinearTeacher(l, LINEAR_W)
             X_train, Y_train = plteacher.generate_dataset(TRAINING_SET_SIZE, noise=0.1)
             X_test, Y_test = plteacher.generate_dataset(TESTING_SET_SIZE, noise=0)
-            initial_params = pnp.random.uniform(low=-0.01, high=0.01, size=(l,))
-            optimizer = AdamOptimizer()
-            specs, trace = plteacher.evaluate(initial_params, optimizer, MAX_EPOCHS, X_train, Y_train, X_test, Y_test)
+            initial_params = jax.random.uniform(plteacher.rng, minval=-0.01, maxval=0.01, shape=(l,))
+            specs, trace = plteacher.evaluate(initial_params, MAX_EPOCHS, X_train, Y_train, X_test, Y_test)
             json.dump(specs, open(f"linear-teacher-experiments-{v}/specs_{l}.json", "w"))
             trace.to_pickle(f"linear-teacher-experiments-{v}/trace_{l}.pickle")
-            training_loss = trace.loc[len(trace) - 1].training_loss
+            training_loss = trace.loc[len(trace)-1].training_loss
             testing_loss = trace.loc[len(trace) - 1].testing_loss
             print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')} {v}: Training loss is: {training_loss:3.3f}; Testing loss is: {testing_loss:3.3f}")
             training_losses.append(training_loss)
--- linear-teacher/plot-linear-teacher.py
+++ linear-teacher/plot-linear-teacher.py
@@ -1,3 +1,4 @@
+import matplotlib.colors
 import numpy as np
 import pandas.core.frame
 import pennylane as qml
@@ -7,79 +8,18 @@ import pandas as pd
 from functools import partial
 import matplotlib.pyplot as plt
 import json
+import click
+
+from sklearn.svm import SVR
 
 
-class PennylaneLinearTeacher:
-
-    def __init__(self, n_layers, linear_w):
-
-        self.n_layers = n_layers
-        self.device = qml.device("default.qubit", wires=1)
-
-        @qml.qnode(self.device)
-        def one_qubit_learner(x, theta):
-            qml.RY(x, wires=0)
-            for i, thetai in enumerate(theta):
-                if i % 2 == 0:
-                    qml.RZ(thetai, wires=0)
-                else:
-                    qml.RX(thetai, wires=0)
-            return qml.expval(qml.PauliZ(0))
-
-        self.linear_w = linear_w
-        self.one_qubit_learner = one_qubit_learner
-
-    def generate_dataset(self, size, noise=0):
-        X = pnp.random.uniform(low=0, high=1, size=(size,))
-        Y = X * self.linear_w + pnp.random.uniform(low=-noise, high=+noise, size=(size,))
-        return X, Y
-
-    def evaluate(self, initial_params, optimizer, epochs, X_train, Y_train, X_test, Y_test):
-
-        specs = {'initial_params': str(initial_params),
-                 'optimizer': str(type(optimizer)),
-                 'epochs': epochs,
-                 'layers': self.n_layers,
-                 'linear_w': self.linear_w,
-                 'X_train': str(X_train),
-                 'Y_train': str(Y_train),
-                 'X_test': str(X_test),
-                 'Y_test': str(X_test)}
-
-        df = pd.DataFrame(columns=['epoch', 'training_loss', 'testing_loss', 'params'])
-
-        def get_mse_loss(X, Y, params):
-            return sum((self.one_qubit_learner(X[i], params) - Y[i])**2 for i in range(X.shape[0]))
-
-        training_loss = partial(get_mse_loss, X_train, Y_train)
-        testing_loss = partial(get_mse_loss, X_test, Y_test)
-        params = initial_params
-
-        for i in range(epochs+1):
-
-            if i > 0: params = optimizer.step(training_loss, params)
-
-            df.loc[len(df)] = {
-                'epoch': i,
-                'training_loss': training_loss(params),
-                'testing_loss': testing_loss(params),
-                'params': params
-            }
-
-        return specs, df
-
-
-TRAINING_SET_SIZE = 6
-TESTING_SET_SIZE = 20
-MAX_DEPTH = 60
-LINEAR_W = 0.66
-MAX_EPOCHS = 1000
 
 training_losses = []
 testing_losses = []
 
-def load_trace(i, v=1):
-    return pd.read_pickle(f"linear-teacher-experiments-{v}/trace_{i}.pickle")
+
+
+
 
 def print_plot(tr_losses, te_losses, xlog, vaxis=None):
     assert len(tr_losses) == len(te_losses)
@@ -90,8 +30,10 @@ def print_plot(tr_losses, te_losses, xlog, vaxis=None):
     if vaxis is not None: plt.axvline(x=vaxis, color='r')
     plt.show()
 
+
 def print_plot_depth(dfs, depth, xlog=False):
     print_plot(dfs[depth].training_loss, dfs[depth].testing_loss, xlog)
+
 
 def get_training_loss(df, i):
     if type(i) == list:
@@ -99,11 +41,13 @@ def get_training_loss(df, i):
     else:
         return df.loc[i].training_loss
 
+
 def get_testing_loss(df, i):
     if type(i) == list:
         return sum(df.loc[ii].testing_loss for ii in i) / len(i)
     else:
         return df.loc[i].testing_loss
+
 
 def print_plot_epoch(dfs, i, xlog=False, vaxis=None):
     tr_losses = []
@@ -113,6 +57,7 @@ def print_plot_epoch(dfs, i, xlog=False, vaxis=None):
         te_losses.append(get_testing_loss(df, i))
     print_plot(tr_losses, te_losses, xlog, vaxis)
 
+
 def create_heatmap(dfs):
     train_loss_hm = pnp.zeros((len(dfs), len(dfs[0].training_loss)))
     test_loss_hm = pnp.zeros((len(dfs), len(dfs[0].training_loss)))
@@ -120,6 +65,20 @@ def create_heatmap(dfs):
         train_loss_hm[i] = df.training_loss.to_numpy()
         test_loss_hm[i] = df.testing_loss.to_numpy()
     return train_loss_hm, test_loss_hm
+
+
+def create_params_norm_change(dfs):
+    EPOCHS = len(dfs[0].params)
+    params_change = pnp.zeros((len(dfs), EPOCHS))
+    for i, df in enumerate(dfs):
+        P = df.loc[0].params
+        NP = np.linalg.norm(P)
+        def normalise(x):
+            return np.linalg.norm(x - P) / NP
+        df_norm = np.vectorize(normalise)(df.params.to_numpy())
+        params_change[i] = df_norm
+    params_change[params_change < 1e-2] = 1e-2
+    return params_change
 
 
 def print_heatmap_log(dfs):
@@ -143,5 +102,254 @@ def print_heatmap_log(dfs):
     plt.axvline(x=4, color='r')
     plt.show()
 
-dfs1 = [load_trace(i + 1, v=1) for i in range(MAX_DEPTH)]
-dfs2 = [load_trace(i + 1, v=2) for i in range(MAX_DEPTH)]
+
+
+def params_norm_change_log(dfs):
+
+    if type(dfs[0]) == pandas.core.frame.DataFrame:
+        params_hm = create_params_norm_change(dfs)
+    else:
+        N = len(dfs)
+        params_hm = create_params_norm_change(dfs[0])
+        for i in range(1, N):
+            params_hm += create_params_norm_change(dfs[i])
+        params_hm /= N
+
+    im = plt.pcolor(params_hm.T, norm=matplotlib.colors.LogNorm(vmin=1e-2))
+    plt.colorbar(im, orientation='horizontal')
+    plt.yscale('log')
+    plt.ylim((1, 1000))
+    plt.axvline(x=4, color='r')
+    plt.show()
+
+# ======================================================================================================
+
+TRAINING_SET_SIZE = 6
+TESTING_SET_SIZE = 20
+MAX_REP = 15
+MAX_DEPTH = 60
+LINEAR_W = 0.66
+MAX_EPOCHS = 1001
+DATAFRAMES = []
+DATAFRAMES.append([])  # the zero-th repetition is empty
+
+
+def load_trace(rep, layers):
+    return pd.read_pickle(f"linear-teacher-experiments-{rep}/trace_{layers}.pickle")
+
+
+def load_all_traces():
+    print("Loading dataframes...")
+    for rep in range(1, MAX_REP + 1):
+        DATAFRAMES.append([])
+        for layers in range(1, MAX_DEPTH + 1):
+            df = load_trace(rep=rep, layers=layers)
+            DATAFRAMES[rep].append(df)
+    print("Loaded!")
+
+@click.group()
+def main():
+    """Simple CLI for querying books on Google Books by Oyetoke Toby"""
+    pass
+
+
+@main.command()
+@click.option('--repetition', default=-1, type=int)
+def plotnormchangeheatmap(repetition):
+    """
+    Plot the heatmap related in norm change
+
+    REPETITION is which repetition you want the plot of (-1 for the average)
+    """
+    load_all_traces()
+
+    def create_norm_change_heatmap(df_nested_list):
+        # create empty heatmap
+        params_change = pnp.zeros((MAX_DEPTH, MAX_EPOCHS))
+        # for each "depth" dataframe
+        for i, df in enumerate(df_nested_list):
+            init_params = df.loc[0].params
+            init_params_norm = np.linalg.norm(init_params)
+
+            def normalise(actual_params):
+                return np.linalg.norm(actual_params - init_params) / init_params_norm
+
+            df_norm = np.vectorize(normalise)(df.params.to_numpy())
+            params_change[i] = df_norm
+        params_change[params_change < 1e-2] = 1e-2
+        return params_change
+
+    if repetition == -1:  # average of all repetitions
+        heatmap_matrix = np.average([create_norm_change_heatmap(DATAFRAMES[i]) for i in range(MAX_REP)], axis=0)
+    else:  # a single, specific repetition
+        heatmap_matrix = create_norm_change_heatmap(DATAFRAMES[repetition])
+
+    im = plt.pcolor(heatmap_matrix.T, norm=matplotlib.colors.LogNorm(vmin=1e-2))
+    plt.title(f"Norm change ({'Average of all repetitions' if repetition==-1 else f'repetition {repetition}'})")
+    plt.colorbar(im, orientation='horizontal')
+    plt.ylabel('Epochs of training')
+    plt.yscale('log')
+    plt.ylim((1, 1000))
+    plt.xlabel('Depth of circuits')
+    plt.axvline(x=4, color='r')
+    plt.show()
+
+
+@main.command()
+@click.option('--repetition', default=-1, type=int)
+@click.option('--epoch', default=0, type=int)
+@click.option('--depth', default=0, type=int)
+def plotnormchange(repetition, epoch, depth):
+    """
+    Plot the norm change
+
+    REPETITION is which repetition you want the plot of (-1 for the average)
+    """
+    if epoch == depth:
+        raise ValueError("You must set either EPOCH or DEPTH")
+    raise ValueError("TODO")
+    load_all_traces()
+
+
+def s2np(s):
+    return np.array(s[1:-1].split(), dtype=float)
+
+
+def target_kernel_alignment(y1, y2, K):
+    """Calculate kernel polarity"""
+    y1 = y1.reshape((len(y1), 1))
+    y2 = y2.reshape((1, len(y2)))
+    Y = np.matmul(y1, y2)
+    polarity = np.sum(Y * K)
+    norm_Y = np.linalg.norm(Y)
+    norm_K = np.linalg.norm(K)
+    target_alignment = polarity / (norm_K * norm_Y)
+    return target_alignment
+
+
+def svr_loss(K_train, Y_train, K_test, Y_test):
+    regr = SVR(kernel='precomputed')
+    regr.fit(K_train.T, Y_train)
+    Y_actual = regr.predict(K_test.T)
+    return np.linalg.norm(Y_actual - Y_test)
+
+
+def load_ntk_data(repetition, layers, epoch):
+    specs = json.load(open(f"linear-teacher-experiments-{repetition}/specs_{layers}.json"))
+    X_train, X_test = s2np(specs["X_train"]), s2np(specs["X_test"])
+    Y_train, Y_test = s2np(specs["Y_train"]), s2np(specs["Y_test"])
+    ntk_training_grams = np.load(f"linear-teacher-experiments-{repetition}/ntk_training_gram_{layers}.npy")
+    ntk_testing_grams = np.load(f"linear-teacher-experiments-{repetition}/ntk_testing_gram_{layers}.npy")
+    if epoch == -1:
+        K_train = np.average(ntk_training_grams, axis=2)
+        # print(ntk_training_grams.shape, "=>", K_train.shape)
+        K_test = np.average(ntk_testing_grams, axis=2)
+        # print(ntk_testing_grams.shape, "=>", K_test.shape)
+    else:
+        K_train = ntk_training_grams[:, :, epoch]
+        K_test = ntk_testing_grams[:, :, epoch]
+    return X_train, K_train, Y_train, X_test, K_test, Y_test
+
+
+@main.command()
+@click.option('--repetition', default=-1, type=int)
+@click.option('--epoch', default=0, type=int)
+def plotntktargetalignment(repetition, epoch):
+    """
+    Plot the norm change
+
+    REPETITION is which repetition you want the plot of (-1 for the average)
+    """
+    if repetition != 1:
+        raise ValueError("Not simulated yet")
+
+    MAX_DEPTH_NTK = 28
+
+    train_alignment = []
+    test_alignment = []
+
+    for layers in range(1, MAX_DEPTH_NTK+1):
+        _, K_train, Y_train, _, K_test, Y_test = load_ntk_data(repetition, layers, epoch)
+        train_alignment.append(target_kernel_alignment(Y_train, Y_train, K_train))
+        test_alignment.append(target_kernel_alignment(Y_train, Y_test, K_test))
+
+    plt.title(f"Target-Kernel alignment using NTK at epoch {epoch}")
+    plt.plot(range(1, MAX_DEPTH_NTK+1), train_alignment, label="Training")
+    plt.plot(range(1, MAX_DEPTH_NTK+1), test_alignment, label="Testing")
+    plt.legend()
+    plt.show()
+
+
+@main.command()
+@click.option('--repetition', default=-1, type=int)
+@click.option('--epoch', default=0, type=int)
+def plotntksvrloss(repetition, epoch):
+    """
+    Plot the norm change
+
+    REPETITION is which repetition you want the plot of (-1 for the average)
+    """
+    if repetition != 1:
+        raise ValueError("Not simulated yet")
+
+    MAX_DEPTH_NTK = 28
+
+    accuracies = []
+
+    for layers in range(1, MAX_DEPTH_NTK+1):
+        _, K_train, Y_train, _, K_test, Y_test = load_ntk_data(repetition, layers, epoch)
+        accuracies.append(svr_loss(K_train, Y_train, K_test, Y_test))
+
+    plt.title(f"SVR loss using NTK at epoch {epoch} - lower is better")
+    plt.plot(range(1, MAX_DEPTH_NTK+1), accuracies, label="Loss")
+    plt.legend()
+    plt.show()
+
+
+@main.command()
+@click.option('--repetition', default=-1, type=int)
+@click.option('--epoch', default=0, type=int)
+@click.option('--layers', default=1, type=int)
+def plotntkprediction(repetition, epoch, layers):
+    """
+    Plot the norm change
+
+    REPETITION is which repetition you want the plot of (-1 for the average)
+    """
+    MAX_DEPTH_NTK = 28
+
+    if repetition != 1:
+        raise ValueError("Not simulated yet")
+    if layers not in range(1, MAX_DEPTH_NTK+1):
+        raise ValueError("Not simulated yet")
+
+    X_train, K_train, Y_train, X_test, K_test, Y_test = load_ntk_data(repetition, layers, epoch)
+    regr = SVR(kernel='precomputed')
+    regr.fit(K_train.T, Y_train)
+    Y_actual = regr.predict(K_test.T)
+
+    plt.title(f"SVR loss using NTK at epoch {epoch} - lower is better")
+    print(X_train.tolist())
+    print(Y_train.tolist())
+    print(X_test.tolist())
+    print(Y_test.tolist())
+    plt.plot([0,1], [0,0.66], color='red')
+    plt.scatter(X_train.tolist(), Y_train.tolist(), label="Training")
+    plt.scatter(X_test.tolist(), Y_test.tolist(), label="Testing (ideal)")
+    plt.scatter(X_test.tolist(), Y_actual.tolist(), label="Testing (actual)")
+    plt.legend()
+    plt.show()
+
+
+@main.command()
+def fixspecs():
+    """
+    Plot the norm change
+
+    REPETITION is which repetition you want the plot of (-1 for the average)
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
